53976aadee8774c3e68c808d688f76fe23decd9e
thore <t.mueller@onlineprinters.com>
2025-07-17 17:21:23 +0200

    add ssh key

--- modules/user.nix
+++ modules/user.nix
@@ -3,15 +3,18 @@
   users.users = {
     fablab = {
       isNormalUser = true;
-      extraGroups = [ "dialout" "netdev" "widgetkit" "video" ... ];
+      extraGroups = [ "dialout" "netdev" "widgetkit" "video" ];
       shell = pkgs.zsh;
       password = config.lib.mkDefault config.fablab.passwordHash;
     };
     fabadm = {
       isNormalUser = true;
       isRoot = false;
-      extraGroups = [ "wheel" "dialout" "netdev" ... ];
+      extraGroups = [ "wheel" "dialout" "netdev" ];
       shell = pkgs.zsh;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAAnjPAjJolbiamuD7BydWbvLMtoId+4JCJ6i1Hokfkr thore@fablab"
+      ];
       password = config.lib.mkDefault config.fabadm.passwordHash;
     };
   };
